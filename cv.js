df3dca6dd4790bb86b1d16a86fe6cedd2d100a3d
VuND <vunguyenbn97@gmail.com>
2024-03-27 00:27:02 +0700

    Done my cv

---
+++ cv.js
@@ -0,0 +1,301 @@
+const data = {
+    full_name: "Nguyen Duc Vu",
+    title: "PHP Developer",
+    info_contacts: [
+        {
+            icon: "fa-solid fa-envelope",
+            action: "mailto:vunguyenbn97@gmail.com",
+            text: "vunguyenbn97@gmail.com",
+        },
+        {
+            icon: "fa-solid fa-mobile-screen-button",
+            action: "tel:0977468402",
+            text: "0977 468 402",
+        },
+        {
+            icon: "fa-solid fa-location-dot",
+            action: "#",
+            text: "Ha Noi",
+        },
+        {
+            icon: "fa-brands fa-linkedin",
+            action: "https://linkedin.com/in/vund-dev",
+            text: "https://linkedin.com/in/vund-dev",
+        },
+        {
+            icon: "fa-brands fa-github",
+            action: "https://github.com/vund-dev",
+            text: "https://github.com/vund-dev",
+        },
+    ],
+    objectives: {
+      title: 'CAREER OBJECTIVES',
+      items: [
+          'I have more than 4 years of software development experience in OutSource and Product projects.',
+          'OutSource projects develop image processing applications, vehicle recognition, IoT applications, and e-commerce for the Japanese market.',
+          'Currently, I am working at TopCV Vietnam Joint Stock Company with products for candidates and employers.',
+          'My upcoming goal is to work at professional technology companies. I want to learn more from experienced colleagues, contribute to the company as well as develop myself to become a Senior Developer or Technical Leader.',
+      ],
+    },
+    educations: {
+
+    },
+    projects: [
+        {
+            name: 'Traffic Guide Application',
+            time: '10/2019 - 06/2021',
+            details: [
+                {
+                    label: 'Customer',
+                    text: 'Japan',
+                },
+                {
+                    label: 'Description',
+                    text: 'Solve the problem of traffic guide at road maintenance construction sites',
+                },
+                {
+                    label: 'Team Size',
+                    text: 5,
+                },
+                {
+                    label: 'Positions',
+                    text: [
+                        'Fresher Developer (10/2019 - 01/2020)',
+                        'Junior Developer (01/2020 - 06/2021)'
+                    ],
+                },
+                {
+                    label: 'Responsibilities',
+                    text: [
+                        'Develop lane recognition service',
+                        'Training model, improving the accuracy of identifying vehicles and license plates',
+                        'Fix errors that occur during actual runtime and optimize performance',
+                        'Develop web service to support remote object location',
+                        'Coordinate with BrSE in Japan when installing experimental products at off-road construction sites',
+                    ],
+                },
+                {
+                    label: 'Technologies',
+                    text: 'C++, Python, OpenCV, Yolo, Laravel, HTML, CSS, JQuery',
+                },
+            ],
+        },
+        {
+            name: 'Wedding E-commerce',
+            time: '02/2020 - 06/2021',
+            details: [
+                {
+                    label: 'Customer',
+                    text: 'Japan',
+                },
+                {
+                    label: 'Description',
+                    text: 'Online weddings are becoming more and more popular, celebrating wedding guests is easier with online shopping services according to the couple\'s wishes.',
+                },
+                {
+                    label: 'Team Size',
+                    text: 5,
+                },
+                {
+                    label: 'Positions',
+                    text: 'Junior Developer'
+                },
+                {
+                    label: 'Responsibilities',
+                    text: [
+                        'Participate in building and designing Database for features in the project',
+                        'Develop backend and frontend product modules',
+                    ],
+                },
+                {
+                    label: 'Technologies',
+                    text: 'Laravel, MySQL, HTML, CSS, Jquery',
+                },
+            ],
+        },
+        {
+            name: 'Topcv.vn',
+            time: '06/2021 - 09/2021',
+            details: [
+                {
+                    label: 'Company',
+                    text: 'TopCV VietNam JSC',
+                },
+                {
+                    label: 'Description',
+                    text: 'One of the Company\'s core products with services such as supporting free online CV creation tools, finding jobs for candidates and some other features.',
+                },
+                {
+                    label: 'Team Size',
+                    text: 5,
+                },
+                {
+                    label: 'Positions',
+                    text: 'Junior Developer'
+                },
+                {
+                    label: 'Responsibilities',
+                    text: [
+                        'Participate in developing cv templates',
+                        'Develop candidate vip feature',
+                        'Optimize and increase candidate experience',
+                    ],
+                },
+                {
+                    label: 'Technologies',
+                    text: 'Laravel, MySQL, Elasticsearch, HTML, CSS, JQuery,...',
+                },
+            ],
+        },
+        {
+            name: 'Internal Operation System',
+            time: '09/2021 - 03/2023',
+            details: [
+                {
+                    label: 'Company',
+                    text: 'TopCV VietNam JSC',
+                },
+                {
+                    label: 'Description',
+                    text: 'Products operated internally by the Company. Has main features such as: Workflow, Customer Tracking, Human Resource Management, CRM,...',
+                },
+                {
+                    label: 'Team Size',
+                    text: 'Agile Team 10',
+                },
+                {
+                    label: 'Positions',
+                    text: [
+                        'Junior Developer (09/2021 - 04/2022)',
+                        'Middle Developer (04/2022 - 03/2023)',
+                    ],
+                },
+                {
+                    label: 'Responsibilities',
+                    text: [
+                        'Participate in building and developing professional features to support sales and customer care staff.',
+                        'Participate in building process systems and managing Workflow processes.',
+                        'Participate in building Microservices about: Accounts, Customers, Human Resources, Workflow,...',
+                        'Participate in building and designing Database for features in the project.',
+                        'Optimize the experience in the system.',
+                        'Join the Agile team in evaluating and contributing ideas to build new features.',
+                    ],
+                },
+                {
+                    label: 'Technologies',
+                    text: 'Laravel, NestJS, ReactJS, Microservices Architecture, Domain-Driven Design (DDD), Kafka, Elasticsearch, MySQL, MongoDB',
+                },
+            ],
+        },
+        {
+            name: 'Tuyendung.topcv.vn',
+            time: '03/2023 - now',
+            details: [
+                {
+                    label: 'Company',
+                    text: 'TopCV VietNam JSC',
+                },
+                {
+                    label: 'Description',
+                    text: 'Smart recruitment platform for employers, supporting finding suitable candidates. Apply AI technology to products. Main features such as: Recruitment campaign, Post job ads, Find CV, Recommend CV, Manage CV, Provide paid service packages for employers,...',
+                },
+                {
+                    label: 'Team Size',
+                    text: 'Agile Team 11',
+                },
+                {
+                    label: 'Positions',
+                    text: 'Middle Developer'
+                },
+                {
+                    label: 'Responsibilities',
+                    text: [
+                        'Participate in developing features such as Posting job advertisements, Searching CVs, Recommend CV, Managing CVs, including interfaces, APIs, Databases.',
+                        'Optimize queries, refactor code',
+                        'Resolve bugs that arise in the production environment',
+                        'Join the Agile team in evaluating and contributing ideas to build new features.',
+                    ],
+                },
+                {
+                    label: 'Technologies',
+                    text: 'Laravel, VueJS, SCSS, React Native, MySQL, MongoDB, Elasticsearch, Kafka, Microservices Architecture, Domain-Driven Design',
+                },
+            ],
+        },
+    ],
+}
+
+fillData();
+function fillData() {
+    fillNameAndTitle()
+    fillContact()
+    fillObjective()
+    fillProjects()
+}
+
+function fillNameAndTitle() {
+    const { full_name, title } = data
+    document.getElementById('full_name').innerText = full_name
+    document.getElementById('title').innerText = title
+}
+
+function fillContact() {
+    let html = ''
+    const { info_contacts } = data
+    info_contacts.forEach((info) => {
+        html += '<div class="info-contact-item">'
+        html += `<div class="icon-info"><i class="${info.icon}"></i></div>`
+        html += `<a href="${info.action}">${info.text}</a>`
+        html += '</div>'
+    })
+    document.getElementById('info-contact').innerHTML = html
+}
+
+function fillObjective() {
+    const { objectives } = data
+    let html = '<div class="title-block">'
+    html += objectives.title
+    html += '</div>'
+    html += '<div class="content-block">'
+
+    objectives.items.forEach((item) => {
+        html += `<span class="mb-6">${item}</span>`
+    })
+    html += '</div>'
+    document.getElementById('about').innerHTML = html
+}
+
+function fillProjects() {
+    let html = '<div class="title-block">PROJECTS</div>'
+    const { projects } = data
+    projects.forEach((project) => {
+        html += '<div class="content-block">'
+        html += '<div class="flex items-center">'
+        html += `<span class="font-bold text-16">${project.name}</span>`
+        html += `<span class="ml-6">(${project.time})</span>`
+        html += '</div>'
+        html += '<table class="my-12">'
+
+        const { details } = project
+        details.forEach((detail) => {
+            html += '<tr>'
+            html += '<td class="w-250">'
+            html += detail.label
+            html += '</td>'
+            html += '<td>'
+
+            if (detail.text instanceof Array) {
+                html += '<div class="flex-col">'
+                detail.text.forEach((txt) => {
+                    html += `<span>- ${txt}</span>`
+                })
+                html += '</div>'
+            } else {
+                html += detail.text
+            }
+        })
+        html += '</table>'
+        html += '</div>'
+    })
+    document.getElementById('projects-block').innerHTML = html
+}
